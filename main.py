e4bcec57b7ac6694286f4b5ac05d1670c6786585
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-19 18:17:04 +0300

    добавил код для обработки кейса вывода сгенерированной группы пользователей в консоль. Для функции main модуля main

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from src.generate import generate_users
+
+
 def main(cnt_generate_user):
-    pass
+    """Выводит сгенерированных пользователей в консоль в списке"""
+    first_names = input("Введите имена пользователей через запятую").split(",")
+    last_names = input("Введите фамилии пользователей через запятую").split(",")
+    cities = input("Введите названия городов через запятую").split(",")
+
+    generated_users = []
+
+    for cnt in cnt_generate_user:
+        generator = generate_users(first_names, last_names, cities)
+        generated_users.append(generator)
+
+    print(generated_users)
+
+    return None
