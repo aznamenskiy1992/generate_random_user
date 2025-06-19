25aee7ebd00b5f4756640885073a6ccbd1477c38
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-19 15:41:21 +0300

    обработал кейс ошибки, когда аргументы переданы не в списке. Для функции generate_users модуля generate

--- src/generate.py
+++ src/generate.py
@@ -3,6 +3,20 @@ import random
 
 def generate_users(first_names, last_names, cities):
     """Генерирует случайного пользователя из полученных данных и возвращает его в словаре"""
+    args_with_incorrect_type = []
+    args = {
+        "first_names": first_names,
+        "last_names": last_names,
+        "cities": cities,
+    }
+
+    for key, value in args.items():
+        if not isinstance(value, list):
+            args_with_incorrect_type.append(key)
+
+    if len(args_with_incorrect_type) > 0:
+        raise TypeError(f"Аргументы должны быть переданы в списках. Не в списках: {", ".join(args_with_incorrect_type)}")
+
     while True:
         generated_user = {
             "first_name": random.choice(first_names),
