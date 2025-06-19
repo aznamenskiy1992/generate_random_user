7ed035cddb3a4e7b61ad0772b938daaf73a8c3ef
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-19 19:36:57 +0300

    обработал кейс ошибки ввода None вместо параметров пользователя. Для функции main модуля main

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from src.generate import generate_users
 
 def main(cnt_generate_user):
     """Выводит сгенерированных пользователей в консоль в списке"""
-    first_names = input("Введите имена пользователей через запятую").split(",")
-    last_names = input("Введите фамилии пользователей через запятую").split(",")
-    cities = input("Введите названия городов через запятую").split(",")
+    first_names = input("Введите имена пользователей через запятую")
+    last_names = input("Введите фамилии пользователей через запятую")
+    cities = input("Введите названия городов через запятую")
+
+    if None in (first_names, last_names, cities):
+        raise ValueError("Не введены параметры пользователя")
+
+    first_names_split = first_names.split(",")
+    last_names_split = last_names.split(",")
+    cities_split = cities.split(",")
 
     generated_users = []
 
     for cnt in range(cnt_generate_user):
-        generator = generate_users(first_names, last_names, cities)
+        generator = generate_users(first_names_split, last_names_split, cities_split)
         generated_users.append(generator)
 
     print(generated_users)
