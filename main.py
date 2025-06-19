c2946d0bb8d0115f217bb9fc511d5684e038eaf7
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-19 19:45:47 +0300

    проверил модули main и generate линтерами

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+from typing import Iterator
+
 from src.generate import generate_users
 
 
-def main(cnt_generate_user):
+def main(cnt_generate_user: int) -> None:
     """Выводит сгенерированных пользователей в консоль в списке"""
-    first_names = input("Введите имена пользователей через запятую")
-    last_names = input("Введите фамилии пользователей через запятую")
-    cities = input("Введите названия городов через запятую")
+    first_names: str = input("Введите имена пользователей через запятую")
+    last_names: str = input("Введите фамилии пользователей через запятую")
+    cities: str = input("Введите названия городов через запятую")
 
     if None in (first_names, last_names, cities):
         raise ValueError("Не введены параметры пользователя")
 
-    first_names_split = first_names.split(",")
-    last_names_split = last_names.split(",")
-    cities_split = cities.split(",")
+    first_names_split: list = first_names.split(",")
+    last_names_split: list = last_names.split(",")
+    cities_split: list = cities.split(",")
 
-    generated_users = []
+    generated_users: list = []
 
     for cnt in range(cnt_generate_user):
-        generator = generate_users(first_names_split, last_names_split, cities_split)
+        generator: Iterator[dict] = generate_users(first_names_split, last_names_split, cities_split)
         generated_users.append(generator)
 
     print(generated_users)
--- src/generate.py
+++ src/generate.py
@@ -1,21 +1,23 @@
 import random
+from typing import Iterator
 
 
-def generate_users(first_names, last_names, cities):
+def generate_users(first_names: list, last_names: list, cities: list) -> Iterator[dict]:
     """Генерирует случайного пользователя из полученных данных и возвращает его в словаре"""
-    invalid_args = [name for name, arg in zip(
-        ['first_names', 'last_names', 'cities'],
-        [first_names, last_names, cities]
-    ) if not isinstance(arg, list)]
+    invalid_args: list = [
+        name
+        for name, arg in zip(["first_names", "last_names", "cities"], [first_names, last_names, cities])
+        if not isinstance(arg, list)
+    ]
 
     if invalid_args:
         raise TypeError(f"Аргументы должны быть переданы в списках. Не в списках: {", ".join(invalid_args)}")
 
     while True:
-        generated_user = {
+        generated_user: dict = {
             "first_name": random.choice(first_names),
             "last_name": random.choice(last_names),
             "age": random.randint(18, 65),
-            "city": random.choice(cities)
+            "city": random.choice(cities),
         }
         yield generated_user
