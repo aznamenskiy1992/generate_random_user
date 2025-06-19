6304304af5d5ec6a7ec757215f1ac431dee9e563
Artem Znamenskiy <aznamenskiy92@yandex.ru>
2025-06-19 14:26:12 +0300

    дополнил тест кейса по проверке генерации случайного имени проверкой генерации имени, фамилии и города из списков. Для функции generate_users модуля generate

--- test/test_generate.py
+++ test/test_generate.py
@@ -5,17 +5,25 @@ import random
 from src.generate import generate_users
 
 
-@patch('random.choice')
-def test_generate_random_first_name_for_generate_users(mock_random_choice):
-    """Тестирует генерацию случайного имени из полученного списка"""
-    mock_random_choice.return_value = "John"
+@pytest.mark.parametrize(
+    "column, assert_call_arg, result",
+    [
+        ("first_name", ["John", "Mike", "Bob"], "John"),
+        ("last_name", ["Doe", "Kahil", "Birken"], "Doe"),
+        ("city", ["New York", "Los Angeles"], "New York"),
+    ]
+)
+def test_generate_random_first_last_name_and_cities_for_generate_users(column, assert_call_arg, result):
+    """Тестирует генерацию случайного имени, фамилии и города из полученных списков"""
+    with patch('random.choice') as mock_random_choice:
+        mock_random_choice.return_value = result
 
-    generator = generate_users(
-        ["John", "Mike", "Bob"],
-        ["Doe", "Kahil", "Birken"],
-        ["New York", "Los Angeles"]
-    )
+        generator = generate_users(
+            ["John", "Mike", "Bob"],
+            ["Doe", "Kahil", "Birken"],
+            ["New York", "Los Angeles"]
+        )
 
-    assert next(generator)["first_name"] == "John"
+        assert next(generator)[column] == result
 
-    mock_random_choice.assert_any_call(["John", "Mike", "Bob"])
+        mock_random_choice.assert_any_call(assert_call_arg)
